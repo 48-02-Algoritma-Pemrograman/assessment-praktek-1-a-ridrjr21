d09da702c6d06c4af2285dfbcbe6cd735c64698d
faridasyraafrjr <undinevrm21@gmail.com>
2024-10-24 12:47:42 +0700

    Update Nomor2.java

--- Nomor2.java
+++ Nomor2.java
@@ -1,5 +1,23 @@
 public class Nomor2{
     public static void main(String[] args){
+    //input driver
+        System.out.println("masukan nama driver:Beliau/Mereka/Kita ");
+        String driver = scanner.nextLine();
 
+        //input trip per kilometer
+        System.out.println("jarak yang di tempuh: ");
+        int jarak = input.nextInt();
+        int jarak = 0;
+        
+        // menghitung jarak
+        if (jarak <= 11) {
+            jarak = jarak * 13000;
+        } else {
+            jarak = 1 * 13000;
+        }
+
+        //hasil
+        System.out.println("driver: " + driver);
+        System.out.println("total jarak: " + jarak);
     }
 }
